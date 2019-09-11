6642852d4e9d900c006d83b9ef020e089d58b2bd
jacktnp <thanaponpkw@gmail.com>
2019-09-12 02:47:41 +0700

    :update

--- lab3/5/validator.js
+++ lab3/5/validator.js
@@ -1,56 +1,56 @@
 
 function validateForm(){
     /* บัตรประชา่ชน */
-    var idcard = document.querySelector("#idcard");
-    if (idcard.length != 13 || typeof idcard != 'number'){
+    var idcard = document.querySelector("#idcard").value;
+    if (idcard.length != 13 || idcard != parseInt(idcard, 10)){
         alert("กรุณากรอกหมายเลขบัตรประชาชนให้ถูกต้อง");
         return false;
     }
-    
+
     /* ชื่อจริง */
-    var fname = document.querySelector("#fname");
+    var fname = document.querySelector("#fname").value;
     if (fname.length < 2 || fname.length > 20){
         alert("กรุณากรอกชื่อจริงให้ถูกต้อง");
         return false;
     }
 
     /* นามสกุล */
-    var lname = document.querySelector("#lname");
+    var lname = document.querySelector("#lname").value;
     if (lname.length < 2 || lname.length > 30){
         alert("กรุณากรอกนามสกุลให้ถูกต้อง");
         return false;
     }
 
     /* ที่อยู่ */
-    var address = document.querySelector("#address");
+    var address = document.querySelector("#address").value;
     if (address.length < 5){
         alert("กรุณากรอกที่อยู่ให้ถูกต้อง");
         return false;
     }
 
     /* แขวง/ตำบล  */
-    var district = document.querySelector("#district");
+    var district = document.querySelector("#district").value;
     if (district.length < 2 || typeof district != 'string'){
         alert("กรุณากรอก แขวง/ตำบล ให้ถูกต้อง");
         return false;
     }
 
     /* อำเภอ/เขต  */
-    var area = document.querySelector("#area");
+    var area = document.querySelector("#area").value;
     if (area.length < 2 || typeof area != 'string'){
         alert("กรุณากรอก อำเภอ/เขต ให้ถูกต้อง");
         return false;
     }
 
     /* รหัสไปรษณีย์  */
-    var postnum = document.querySelector("#postnum");
+    var postnum = document.querySelector("#postnum").value;
     if (postnum.length != 5){
         alert("กรุณากรอกรหัสไปรษณีย์ให้ถูกต้อง");
         return false;
     }
 
     /* เบอร์โทรศัพท์  */
-    var phone = document.querySelector("#phone");
+    var phone = document.querySelector("#phone").value;
     if (phone.length < 8 || phone.length > 10){
         alert("กรุณากรอกเบอร์โทรศัพท์ให้ถูกต้อง");
         return false;
